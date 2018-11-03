9344414aed3673b1ce0bf09d6b7326897c5ff362
WeronikaPiotrowska <nimbusek123@gmail.com>
2018-11-03 13:32:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/weronika/puzzle/TouchListener.java
@@ -0,0 +1,34 @@
+package com.example.weronika.puzzle;
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by weronika on 03.11.2018.
+ */
+
+public class TouchListener implements View.OnTouchListener {
+    private float xDelta;
+    private float yDelta;
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        float x = motionEvent.getRawX();
+        float y = motionEvent.getRawY();
+        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                xDelta = x - lParams.leftMargin;
+                yDelta = y - lParams.topMargin;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                lParams.leftMargin = (int) (x - xDelta);
+                lParams.topMargin = (int) (y - yDelta);
+                view.setLayoutParams(lParams);
+                break;
+        }
+
+        return true;
+    }
+}
